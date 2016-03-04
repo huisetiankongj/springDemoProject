23404dfc63fc4a92cc890c252aed7406e64c9752
huisetiankonj <421310742@qq.com>
2016-03-04 16:32:19 +0800

    bean name多个值中间、，；隔开的 第一个被用作标识符 其他的（alias1、alias2、alias3）是别名

--- src/test/java/com/czx/bean/TestHello.java
+++ src/test/java/com/czx/bean/TestHello.java
@@ -40,14 +40,19 @@ public class TestHello {
 	
 	/**
 	 * alias bean
+	 * multiple name
+	 * 指定多个name，多个name用“，”、“；”、“ ”分割，
+	 * 第一个被用作标识符，其他的（alias1、alias2、alias3）是别名，所有标识符也必须在Ioc容器中唯一；
 	 */
 	@Test
 	public void aliasBean(){
 		//1、读取配置文件实例化一个IoC容器  
 		ApplicationContext context = new ClassPathXmlApplicationContext("classpath:com/czx/bean/hello.xml");  
 		//2、从容器中获取Bean，注意此处完全"面向接口编程，而不是面向实现" 
-		HelloAPI helloApi = context.getBean("aliasHello", HelloService.class);  
+		HelloAPI helloApi = context.getBean("aliasHello", HelloService.class);
+		HelloAPI helloApi1 = context.getBean("hello2", HelloService.class);
 		//3、执行业务逻辑  
 		helloApi.sayHello();  
+		helloApi1.sayHello();  
 	}
 }
