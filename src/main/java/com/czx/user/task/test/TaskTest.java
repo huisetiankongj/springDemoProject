ff13f68c907014d7c8be8fd03ad87e0059153b57
Administrator <Administrator@WIN-4EE1VO51HSF>
2016-09-07 00:10:42 +0800

    spring-asm包尽量不要引入spring

--- src/main/java/com/czx/user/task/test/TaskTest.java
+++ src/main/java/com/czx/user/task/test/TaskTest.java
@@ -3,6 +3,11 @@ package com.czx.user.task.test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+/**
+ * spring-asm会和其他的包冲突，尽量不要用这个包
+ * @author Administrator
+ *
+ */
 public class TaskTest {
 
 	
